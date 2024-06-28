34d17c27205e7e1c01ef4541369793fc92237f9a
AquasaAziz247 <azizaquasa@gmail.com>
2024-06-29 00:22:47 +0530

    Linked List

---
+++ Linked List/middleNode.java
@@ -0,0 +1,68 @@
+public class middleNode {
+    Node head;
+    class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data=d;
+            next=null;
+        }
+    }
+    //insert at end
+    public void insertAtEnd(int newData){
+        Node newNode = new Node(newData);
+        //if Linked List is empty
+        if(head == null ){
+            head = new Node(newData);
+            return;
+        }
+        //if not empty
+        newNode.next = null;
+        //traverse
+        Node temp = head;
+        while(temp.next != null){
+            temp=temp.next;
+        }
+        temp.next=newNode;
+        return;
+
+    }
+    public void midNode(){
+        Node slow = head , fast = head;
+        while(fast!=null && fast.next!=null ){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        System.out.println("Middle node of the Linked list is : " + slow.data);
+    }
+    //display
+    public void printNode(){
+        Node current = head;
+        while(current != null ){
+            System.out.println(current.data + " ");
+            current=current.next;
+        }}
+    
+    public static void main(String[] args) {
+        middleNode ll = new middleNode();
+
+        ll.insertAtEnd(2);
+        ll.insertAtEnd(4);
+        ll.insertAtEnd(6);
+        ll.insertAtEnd(8);
+        ll.insertAtEnd(10);
+        ll.printNode();
+        System.out.println("After insertion : ");
+        ll.insertAtEnd(12);
+        ll.insertAtEnd(14);
+        ll.printNode();
+
+        ll.midNode();
+
+
+}
+}
+
+    
+    
+
